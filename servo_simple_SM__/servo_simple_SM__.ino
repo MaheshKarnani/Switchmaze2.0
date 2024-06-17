4b737710bd5473ddb0dfe45e42854e9b91d6a69c
MaheshKarnani <36512140+MaheshKarnani@users.noreply.github.com>
2024-06-17 13:47:21 +0100

    Add files via upload

--- servo_simple_SM__/servo_simple_SM__.ino
+++ servo_simple_SM__/servo_simple_SM__.ino
@@ -8,12 +8,6 @@ Servo servo2;     // Servo 2 = door2
 Servo servo3;     // Servo 3 = door3
 #include<Servo.h>
 Servo servo4;     // Servo 4 = door4 
-#include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-  #include <avr/power.h>
-#endif
-Adafruit_NeoPixel pixels1(8, 2, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel pixels2(8, 3, NEO_GRB + NEO_KHZ800);
 
 const int slowness = 6;   //slowness factor, ms wait between 1deg movements, change to set speed
 
@@ -26,15 +20,14 @@ const int servoPin4 = 10;
 const int Pi2Pin_neopix = 4;
 const int Pi2Pin_door3 = 7;
 const int Pi2Pin_door4 = 8;
-boolean light2_dig_const_flag = false;
 
 //door angles
 const int CLOSE_DOOR1 = 179;
-const int OPEN_DOOR1 = 129;
+const int OPEN_DOOR1 = 127;
 const int CLOSE_DOOR2 = 5;
 const int OPEN_DOOR2 = 50;
 const int CLOSE_DOOR3 = 10;
-const int OPEN_DOOR3 = 68;
+const int OPEN_DOOR3 = 65;
 const int CLOSE_DOOR4 = 175;
 const int OPEN_DOOR4 = 120;
 
@@ -49,18 +42,11 @@ int pos4_target = OPEN_DOOR4;
 
 void setup() 
 {
-  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-    clock_prescale_set(clock_div_1);
-  #endif
   Serial.begin(9600);
   servo1.attach(servoPin1);
   servo2.attach(servoPin2);
   servo3.attach(servoPin3);
   servo4.attach(servoPin4);
- 
-  pixels1.begin();
-  pixels2.begin();
-  pinMode(Pi2Pin_neopix, INPUT);
   pinMode(Pi2Pin_door3, INPUT);
   pinMode(Pi2Pin_door4, INPUT);
 }
@@ -79,6 +65,7 @@ void recvInfo()
     Serial.println(receivedChar);    
   }  
 }
+  
 void moveServo() 
 {
   //target commands
@@ -98,47 +85,22 @@ void moveServo()
   {
     pos2_target=CLOSE_DOOR2;
   }
-  //light switches
-  if (receivedChar=='1')
-  {
-    pixels1.clear();
-    pixels1.show();
-  }
-  if (receivedChar=='2')
-  {
-    pixels1.clear();
-    pixels1.show();
-    pixels1.setPixelColor(1, pixels1.Color(0, 0, 5));
-    pixels1.setPixelColor(2, pixels1.Color(0, 0, 5));
-    pixels1.setPixelColor(3, pixels1.Color(0, 0, 5));
-    pixels1.setPixelColor(4, pixels1.Color(0, 0, 5));
-    pixels1.setPixelColor(5, pixels1.Color(0, 0, 5));
-    pixels1.setPixelColor(6, pixels1.Color(0, 0, 5));
-    pixels1.setPixelColor(7, pixels1.Color(0, 0, 5));
-    pixels1.setPixelColor(0, pixels1.Color(0, 0, 5));
-    pixels1.show();
-  }
-  if ((digitalRead(Pi2Pin_neopix) == LOW) && (light2_dig_const_flag)) 
-  {
-    pixels2.clear();
-    pixels2.show();
-    light2_dig_const_flag=false;
-  }
-  if ((digitalRead(Pi2Pin_neopix) == HIGH) && (!light2_dig_const_flag)) 
-  {
-    pixels2.clear();
-    pixels2.show();
-    pixels2.setPixelColor(1, pixels2.Color(0, 0, 5));
-    pixels2.setPixelColor(2, pixels2.Color(0, 0, 5));
-    pixels2.setPixelColor(3, pixels2.Color(0, 0, 5));
-    pixels2.setPixelColor(4, pixels2.Color(0, 0, 5));
-    pixels2.setPixelColor(5, pixels2.Color(0, 0, 5));
-    pixels2.setPixelColor(6, pixels2.Color(0, 0, 5));
-    pixels2.setPixelColor(7, pixels2.Color(0, 0, 5));
-    pixels2.setPixelColor(0, pixels2.Color(0, 0, 5));
-    pixels2.show();
-    light2_dig_const_flag=true;
-  }
+//  if (receivedChar=='e')
+//  {
+//    pos3_target=OPEN_DOOR3;
+//  }
+//  if (receivedChar=='f') 
+//  {
+//    pos3_target=CLOSE_DOOR3;
+//  }
+//  if (receivedChar=='g')
+//  {
+//    pos4_target=OPEN_DOOR4;
+//  }
+//  if (receivedChar=='h') 
+//  {
+//    pos4_target=CLOSE_DOOR4;
+//  }  
 //DOOR 3 Pi2 control
   if ((digitalRead(Pi2Pin_door3) == HIGH)) 
   {
